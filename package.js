80a163f8920e3b8e7d1270781f540c5a41881bfc
Marko Muric <atis.443@gmail.com>
2015-03-25 09:50:42 +0100

    pack structur modification

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'mmuric:google-maps-clusterer-plus',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'A utility libarary for Google Maps marker clustering.',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/mmuric/google-maps-clusterer-plus',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.4.2');
+  api.use('dburles:google-maps');
+  api.addFiles('mmuric:google-maps-clusterer-plus.js');
+  api.export('MarkerClusterer', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('mmuric:google-maps-clusterer-plus');
+  api.addFiles('google-maps-clusterer-plus-tests.js');
+});
